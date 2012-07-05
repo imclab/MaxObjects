a68138c2a1aea0099d708fc50776cf768c4b7eca
Pierre Guillot <guillotpierre6@gmail.com>
2012-07-05 14:25:56 +0200

    pg.delete finished!

--- MaxDATA/pg.delete/pg.delete.c
+++ MaxDATA/pg.delete/pg.delete.c
@@ -3,6 +3,7 @@
 #include "ext_obex.h"
 #include "ext_common.h"
 #include "jpatcher_api.h"
+#include "jgraphics.h"
 #include "ext_strings.h"
 
 typedef struct  _delete
@@ -22,6 +23,7 @@ void *delete_class;
 void delete_assist(t_delete *x, void *b, long m, long a, char *s);
 void delete_bang(t_delete *x);
 void delete_set(t_delete *x, t_symbol *s, long argc, t_atom *argv);
+void delete_delete(t_delete *x, t_symbol *s, long argc, t_atom *argv);
 int delete_patchline(t_delete *x, t_object *patcher, t_object *cible);
 t_max_err delete_notify(t_delete *x, t_symbol *s, t_symbol *msg, void *sender, void *data);
 void *delete_new(t_symbol *s, int argc, t_atom *argv);
@@ -34,8 +36,8 @@ int main()
 	c = class_new("pg.delete", (method)delete_new, (method)delete_free, (short)sizeof(t_delete), 0L, A_GIMME, 0);
 	class_addmethod(c, (method)delete_bang,		"bang",						0);
 	class_addmethod(c, (method)delete_set,		"set",			A_GIMME,	0);
+	class_addmethod(c, (method)delete_delete,	"delete",		A_GIMME,	0);
 	class_addmethod(c, (method)delete_assist,	"assist",		A_CANT,		0);
-	class_addmethod(c, (method)delete_notify,	"notify",		A_CANT,		0);
 	
 	class_register(CLASS_BOX, c);
 	delete_class = c;
@@ -76,62 +78,74 @@ void *delete_new(t_symbol *s, int argc, t_atom *argv)
 void delete_assist(t_delete *x, void *b, long m, long a, char *s)
 {
 	if (m == ASSIST_INLET)
-		sprintf(s,"(Message or Bang)");
+		sprintf(s,"(Message or Bang) Set and/or delete an object type");
 	else 
 	{
-		sprintf(s,"");
+		switch (a) 
+		{	
+			case 0:
+				sprintf(s,"(Nothing) Exclude an object", a);
+			break;
+			case 1:
+				sprintf(s,"(Nothing) Include an object", a);
+			break;
+		}
 	}
 }
 
 void delete_bang(t_delete *x)
 {
-	int i, check;
-	t_object *patcher, *box, *obj;
+	int i, check, nBox;
+	t_object *patcher, *box, *obj, *delBox;
 
 	object_obex_lookup(x, gensym("#P"), &patcher);
 
 	for (box = jpatcher_get_firstobject(patcher), check = 0; box; box = jbox_get_nextobject(box)) 
 	{
 		if(check == 1)
-			object_method(obj, gensym("free"));
+			jpatcher_deleteobj(patcher, (t_jbox *)delBox);
 
 		obj = jbox_get_object(box);
+		delBox = box;
 		check = 0;
-		if (delete_patchline(x, patcher, obj))
+		if(x->f_all == 1 && delete_patchline(x, patcher, obj) != 0)
+			check = 1;
+		else if(delete_patchline(x, patcher, obj) == 2)
+			check = 1;
+		else if(delete_patchline(x, patcher, obj) == 1)
 		{
 			for(i = 0; i < x->f_size; i++)
 			{
-				if(x->f_all == 1)
+				if (object_classname(obj) == x->f_name[i])
 					check = 1;
-				else if (obj && object_classname(obj) == x->f_name[i])
-				{
-					check = 1;
-					post("bah??");
-				}
 			}
 		}
+		
 	}
 	if(check == 1)
-		object_free(obj);
+		jpatcher_deleteobj(patcher, (t_jbox *)delBox);
 }
 
 int delete_patchline(t_delete *x, t_object *patcher, t_object *cible)
 {
-	int i;
+	int i, value;
 	t_object *line, *obj;
 
+	value = 1;
 	for (line = jpatcher_get_firstline(patcher); line; line = jpatchline_get_nextline(line)) 
 	{
-		obj = jpatchline_get_box1(line);
-	
-		if (object_classname(obj) == gensym("pg.delete"))//(obj == &x->f_ob)// && jpatchline_get_outletnum(line) == 0 && cible == jpatchline_get_box2(line))
+		obj = jbox_get_object(jpatchline_get_box1(line));
+		
+		if (obj == &x->f_ob && jpatchline_get_outletnum(line) == 0 && cible == jbox_get_object(jpatchline_get_box2(line)))
 		{
-			post("salut");
-			return 0;
+			value = 0;
+		}
+		else if (obj == &x->f_ob && jpatchline_get_outletnum(line) == 1 && cible == jbox_get_object(jpatchline_get_box2(line)))
+		{
+			value = 2;
 		}
 	}
-	return 1;
-
+	return value;
 }
 
 void delete_set(t_delete *x, t_symbol *s, long argc, t_atom *argv)
@@ -154,12 +168,13 @@ void delete_set(t_delete *x, t_symbol *s, long argc, t_atom *argv)
 	}
 }
 
+void delete_delete(t_delete *x, t_symbol *s, long argc, t_atom *argv)
+{
+	delete_set(x, s, argc, argv);
+	delete_bang(x);
+}
+
 void delete_free(t_delete *x)
 {
 	freebytes(x->f_name, x->f_size * sizeof(t_symbol *));
-}
-
-t_max_err delete_notify(t_delete *x, t_symbol *s, t_symbol *msg, void *sender, void *data)
-{
-	return object_notify(x, s, data);
 }
